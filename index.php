dd2ea1821a4bcf26e1c51ba3cc8eb9d2e9310779
Dipen <dipendraboyaju222@gmail.com>
2025-02-21 18:55:54 +0545

    change

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>EB Pearls</title>
+  <link rel="stylesheet" href="./css/style.css" />
+  <script defer>
+    function toggleMenu() {
+      document.querySelector(".menu").classList.toggle("active");
+    }
+  </script>
+</head>
+<body>
+
+  <nav class="header">
+    <div class="logo">
+      <img src="./images/logo.png" alt="">
+    </div>
+
+    <div class="menu-icon" onclick="toggleMenu()">☰</div>
+    <div class="menu">
+      <a href="#">Home</a>
+      <a href="#">Solutions</a>
+      <a href="#">Help Center</a>
+      <a href="#">Get Started</a>
+      <div class="buttonss">
+    <button class="login-button">Login</button>
+    <button class="request-button">Request a demo</button>
+    </div>
+    </div>
+    <div class="buttons">
+    <button class="login-button">Login</button>
+    <button class="request-button">Request a demo</button>
+    </div>
+  </nav>
+
+  <section class="hero">
+    <h2>Get instant cash flow with invoice factoring</h2>
+    <p>Why wait? Get same day funding and a faster way to access cash flow.</p>
+    <button class="started-button">Get Started</button>
+  </section>
+
+  <section class="outsource">
+    <div class="title">
+      <h2>Outsource payment collection</h2>
+      <p>Faster and flexible access to cash flow from one or all your invoices.</p>
+    </div>
+    <div class="source-items">
+      <div class="source-item">
+        <img src="./images/Earth.png" alt="">
+        <h4>Access up to $100,000</h4>
+        <p>We fund each invoice once approved and collect payment to optimise your cash flow.*
+        </p>
+      </div>
+      <div class="source-item">
+        <img src="./images/Earth.png" alt="">
+        <h4>Access up to $100,000</h4>
+        <p>We fund each invoice once approved and collect payment to optimise your cash flow.*
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <section class="contact">
+    <div class="contact-title">
+      <h2>Contact Us</h2>
+      <p>Speak with our team to see how we can help your business.</p>
+    </div>
+    <div class="contact-form">
+      <form action="">
+        <input type="text" placeholder="Your name">
+        <input type="email" placeholder="Email">
+        <input type="tel" placeholder="Your best contact number">
+        <textarea name="" id="">Business or company name</textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  </section>
+
+  <footer class="footer">
+    <div class="footer-top">
+      <div class="items">
+       <p>Curabitur consequat, purus a scelerisque sagittis, nulla metus tincidunt elit, vel venenatis nulla libero nec nulla. Suspendisse potenti. Aenean a justo vel sapien pellentesque tincidunt. Sed luctus, elit ac interdum convallis, ligula libero egestas orci, at auctor felis ligula nec odio.</p>
+       <div class="media">
+        <img src="./images/linkedin.png" alt="">
+        <img src="./images/Vector.png" alt="">
+       </div>
+      </div>
+      <div class="items">
+        <h4>Products</h4>
+        <ul>
+          <li>Payments</li>
+          <li>Invoice Factoring</li>
+          <li>Invoice finance</li>
+          <li>Supplier finance</li>
+          <li>Customer finance</li>
+        </ul>
+      </div>
+      <div class="items">
+        <h4>Company</h4>
+        <ul>
+          <li>About us</li>
+          <li>Contact us</li>
+        </ul>
+      </div>
+      <div class="items">
+        <h4>Resources</h4>
+        <ul>
+          <li>Frequently asked questions</li>
+          <li>Knowledge base</li>
+        </ul>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      <div class="">
+        <ul>
+          <li>Privacy Policy</li>
+          <li>Contact us</li>
+        </ul>
+      </div>
+      <div class="">
+        <ul>
+          <li>Site map</li>
+          <li>@ebpearls</li>
+        </ul>
+      </div>
+    </div>
+    </footer>
+</body>
+</html>
